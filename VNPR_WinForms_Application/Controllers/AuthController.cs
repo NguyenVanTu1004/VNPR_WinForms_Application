07f6a66cba979224aa9f0ce84203c2c419ef06c8
Nguyen Van Tu <nguyenvantu03848@gmail.com>
2026-06-25 13:43:58 +0700

    Add project files.

---
+++ VNPR_WinForms_Application/Controllers/AuthController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VNPR_WinForms_Application.Controllers
+{
+    public class AuthController
+    {
+        // Viết hàm xử lý gọi trực tiếp sang tầng Services hoặc Repositories
+    }
+}
---
+++ VNPR_WinForms_Application/Models/Entities/Vehicle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VNPR_WinForms_Application.Models.Entities
+{
+    public class Vehicle
+    {
+        
+    }
+}
